78e70a2baea5675d2c050933eb81746e799407db
GaiaStackX <benniehu@tencent.com>
2017-11-27 21:33:32 +0800

    Update goweb.go

--- goweb.go
+++ goweb.go
@@ -4,22 +4,34 @@ import (
 	"fmt"
 	// "html/template"
 	"io"
+	"os"
+	"log"
 	"net/http"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		io.WriteString(w, "<html><head></head><body><h1>Welcome to Tenxcloud!</h1><img height=\"100\" src=\"/static/logo.png\"></body></html>")
+		io.WriteString(w, "<html><head></head><body><h1>Welcome to GaiaStack!</h1><img height=\"100\" src=\"/static/logo.png\"></body></html>")
 		return
 	}
 }
 
 func main() {
+	logfile,err := os.OpenFile("/data/log/mylog",os.O_RDWR|os.O_CREATE,0666)
+	if err != nil {
+		fmt.Printf("%s\r\n",err.Error())
+		os.Exit(-1)
+	}
+	defer logfile.Close()
+	logger:=log.New(logfile,"INFO:",log.Ldate|log.Ltime|log.Llongfile)
+	logger.Println("hello^^")
+	logger.Println("server begin...")
+
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, r.URL.Path[1:])
 	})
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		fmt.Println("Listen And Server", err.Error())
 	}
